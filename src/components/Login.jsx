28a2ecfb8992bfa38ceb613722607f798553ca70
wiliwan <wiliwan.com@gmail.com>
2019-12-26 15:26:49 +0700

    กเอด้ิื

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <h1>เข้าสู่ระบบ</h1>
+                <h1>เข้าสู่ระบบวิไลวรรณ</h1>
             </div>
         );
     }
