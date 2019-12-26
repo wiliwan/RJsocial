0c9dd8e390e3ed061b527d8457b43ef7a8f881be
it2nb <wootichai_k@hotmail.com>
2019-12-26 14:04:17 +0700

    First Commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h1>เข้าสู่ระบบ</h1>
+            </div>
+        );
+    }
+}
+
+export default Login;
